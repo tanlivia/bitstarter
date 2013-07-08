4d9738b50ce1dcb51ea0d26d4c4dbbc2427a810a
tanlivia <hlh.hust@gmail.com>
2013-07-07 23:27:00 -0700

    web js file wireframe

--- web.js
+++ web.js
@@ -4,10 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var data = fs.readFileSync("index.html", "utf-8");
-    var buffer = new Buffer(100);
-    len = buffer.write(data, "utf-8");
-    response.send(buffer.toString('utf-8', 0, len));
+    var data = fs.readFileSync("index.html");
+    response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
