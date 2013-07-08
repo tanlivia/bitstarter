82bfececd6ce8d17c57dd64311336d4352dd6ca8
tanlivia <hlh.hust@gmail.com>
2013-07-07 22:43:44 -0700

    finish part 1

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var data = fs.readFileSync("index.html", "utf-8");
+    var buffer = new Buffer(100);
+    len = buffer.write(data, "utf-8");
+    response.send(buffer.toString('utf-8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
